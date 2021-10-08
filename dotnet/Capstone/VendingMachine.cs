a81f64273fb623246555573c8349ed9b58952bca
Kasimierz Stepnowski <kstepnowski22@gmail.com>
2021-10-08 18:30:21 -0400

    Mod-1 capstone progress 6.1

--- dotnet/Capstone/VendingMachine.cs
+++ dotnet/Capstone/VendingMachine.cs
@@ -9,6 +9,8 @@ namespace Capstone
 
         //Class Specific variables
         private string mainMenuInput = "";
+        private string displayMenuInput = "";
+        private string moneySelection = "";
 
         //Class Properties
         private List<VendingMachineItem> Inventory { get; } = new List<VendingMachineItem>();
@@ -37,7 +39,15 @@ namespace Capstone
             
         }
 
-        public void PrintInventory()
+        private void PrintHeader()
+        {
+            Console.Clear();
+            Console.WriteLine("********VENDO-MATIC 800********");
+            Console.WriteLine("PLEASE CHOOSE FROM THE FOLLOWING");
+            Console.WriteLine();
+        }
+
+        private void PrintInventory()
         {
             Console.WriteLine("***********************************************");
             Console.WriteLine("***LOCATION  ~  NAME  ~  PRICE  ~  REMAINING***");
@@ -56,17 +66,14 @@ namespace Capstone
 
         private void MainMenuDisplay()
         {
-            Console.Clear();
-            Console.WriteLine("********VENDO-MATIC 800********");
-            Console.WriteLine("PLEASE CHOOSE FROM THE FOLLOWING");
-            Console.WriteLine();
+            PrintHeader();
             Console.WriteLine("(1) Display Vending Machine Items");
             Console.WriteLine("(2) Purchase");
             Console.WriteLine("(3) Exit");
-            mainMenuInput = Console.ReadLine();
+           
         }
 
-        private void MainMenuOption1()
+        private void DisplayInventory()
         {
             
             Console.Clear();
@@ -74,6 +81,75 @@ namespace Capstone
             Console.WriteLine();
             Console.WriteLine("Press ENTER To Return To Main Menu");
             Console.ReadLine();
+        }
+        private void DisplayPurchaseMenu()
+        {
+            PrintHeader();
+            Console.WriteLine("(1) Feed Money");
+            Console.WriteLine("(2) Select Product");
+            Console.WriteLine("(3) Finish Transaction");
+            Console.WriteLine();
+            Console.WriteLine($"Current Money Provided: {Balance}");
+            displayMenuInput = Console.ReadLine();
+
+
+            if (displayMenuInput == "1")
+            {
+                FeedMoneyDisplay();   
+            } 
+
+
+
+
+
+        }
+        private void FeedMoneyDisplay()
+        {
+            PrintHeader();
+            Console.WriteLine("ALL CHANGE RETURNEED IN COINS!");
+            Console.WriteLine("(1) $1");
+            Console.WriteLine("(2) $2");
+            Console.WriteLine("(3) $5");
+            Console.WriteLine("(4) $10");
+            Console.WriteLine("(5) $20");
+            Console.WriteLine("(6) EXIT to main menu");
+            moneySelection = Console.ReadLine();
+            
+            if(moneySelection == "1")
+            {
+                FeedMoney(1);
+            }
+            else if(moneySelection == "2")
+            {
+                FeedMoney(2);
+            }
+            else if(moneySelection == "3")
+            {
+                FeedMoney(5);
+            }
+            else if(moneySelection == "4")
+            {
+                FeedMoney(10);
+            }
+            else if(moneySelection == "5")
+            {
+                FeedMoney(20);
+            }
+            else if(moneySelection == "6")
+            {
+                MainMenuDisplay();
+            }
+            else
+            {
+                Console.WriteLine("Input not accepted! Please press ENTER to continue");
+                Console.ReadLine();
+                FeedMoneyDisplay();
+            }
+           
+        }
+        private void FeedMoney(decimal money)
+        {
+
         }
 
 
@@ -84,7 +160,7 @@ namespace Capstone
         public void Run()
         {
             bool isRunning = true;
-            bool isValidMainMenuInput = false;
+            
            
             
 
@@ -92,23 +168,27 @@ namespace Capstone
             while (isRunning)
             {
                 MainMenuDisplay();
-              
 
-                while (!isValidMainMenuInput)
-                {
-                    if (mainMenuInput == "1")
+
+
+                mainMenuInput = Console.ReadLine();
+                if (mainMenuInput == "1")
                     {
-                        isValidMainMenuInput = true;
-                        MainMenuOption1();
+                        
+                        DisplayInventory();
                     }
                     else if(mainMenuInput == "2")
                     {
-                        isValidMainMenuInput = true;
+                        DisplayPurchaseMenu();
+                    }
+                    else
+                    {
+                    isRunning = false;
                     }
 
                   
-                }
-                isValidMainMenuInput = false;
+                
+                
             }
 
         }
