c016a8d63de7feb7a6ed41f7fbae650d87353d2f
Kasimierz Stepnowski <kstepnowski22@gmail.com>
2021-10-08 16:09:40 -0400

    mod -1 Capstone main menu 1 option  running

--- dotnet/Capstone/VendingMachine.cs
+++ dotnet/Capstone/VendingMachine.cs
@@ -7,6 +7,9 @@ namespace Capstone
     public class VendingMachine
     {
 
+        //Class Specific variables
+        private string mainMenuInput = "";
+
         //Class Properties
         private List<VendingMachineItem> Inventory { get; } = new List<VendingMachineItem>();
         public Dictionary<string, int> Stock { get; private set; } = new Dictionary<string, int>();
@@ -34,7 +37,7 @@ namespace Capstone
             
         }
 
-        public void printInventory()
+        public void PrintInventory()
         {
             foreach(VendingMachineItem item in Inventory)
             {
@@ -44,5 +47,65 @@ namespace Capstone
 
 
         }
+
+        private void MainMenuDisplay()
+        {
+            Console.Clear();
+            Console.WriteLine("********VENDO-MATIC 800********");
+            Console.WriteLine("PLEASE CHOOSE FROM THE FOLLOWING");
+            Console.WriteLine();
+            Console.WriteLine("(1) Display Vending Machine Items");
+            Console.WriteLine("(2) Purchase");
+            Console.WriteLine("(3) Exit");
+            mainMenuInput = Console.ReadLine();
+        }
+
+        private void MainMenuOption1()
+        {
+            
+            Console.Clear();
+            PrintInventory();
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER To Return To Main Menu");
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+        public void Run()
+        {
+            bool isRunning = true;
+            bool isValidMainMenuInput = false;
+           
+            
+
+
+            while (isRunning)
+            {
+                MainMenuDisplay();
+              
+
+                while (!isValidMainMenuInput)
+                {
+                    if (mainMenuInput == "1")
+                    {
+                        isValidMainMenuInput = true;
+                        MainMenuOption1();
+                    }
+                    else if(mainMenuInput == "2")
+                    {
+                        isValidMainMenuInput = true;
+                    }
+
+                  
+                }
+                isValidMainMenuInput = false;
+            }
+
+        }
     }
+
 }
